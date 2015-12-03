692a5bf2d367db52b30466ee35a5ccc32dfef378
jmpaddock <jpadd14@u.washington.edu>
2015-12-02 16:48:11 -0800

    Admin

--- includes/config.php
+++ includes/config.php
@@ -1,120 +1,109 @@
 <?php
 //config.php
 
-define('DEBUG',TRUE); #we want to see all errors
+define('DEBUG',true); #we want to see all errors
+
+define('SECURE',false); #force secure, https, for all site pages
+
+define('PREFIX', 'retro_'); #Adds uniqueness to your DB table names.  Limits hackability, naming collisions
 
 date_default_timezone_set('America/Los_Angeles'); #sets default date/timezone for this website
 
-include 'credentials.php'; //database credentials here.
+/* 
+ *   Virtual (web) 'root' of application for images, JS & CSS files
+ *   
+ *   IF SECURE, MUST BE https://
+ *   Contact hosting company for assistance:
+ *   http://wiki.dreamhost.com/Secure_Hosting
+*/
+define('VIRTUAL_PATH', 'http://example.com/retro/'); 
+
+define('PHYSICAL_PATH', '/home/horsey01/example.com/retro/'); # Physical (PHP) 'root' of application for file & upload reference
+
+# END GENERAL SETTINGS, START BOOTSTRAP CODE ---------------------------
+
+/*
+ * reference required include files here
+ */
+include 'credentials.php'; //stores database login info
 include 'common.php'; //stores all unsightly application functions, etc.
 include 'MyAutoLoader.php'; //loads class that autoloads all classes in include folder
 
-/* automatic path settings - use the following path settings for placing all code in one application folder */ 
-define('VIRTUAL_PATH', 'http://ultan.photography/retro/'); # Virtual (web) 'root' of application for images, JS & CSS files
-define('PHYSICAL_PATH', '/home/ultan/ultan.photography/retro/'); # Physical (PHP) 'root' of application for file & upload reference
+//This defines the current file name
+define('THIS_PAGE',basename($_SERVER['PHP_SELF']));
+
+//force secure website
+if (SECURE && $_SERVER['SERVER_PORT'] != 443) {#force HTTPS
+	header("Location: https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+}
+
 define('INCLUDE_PATH', PHYSICAL_PATH . 'includes/'); # Path to PHP include files - INSIDE APPLICATION ROOT
+
+define('ADMIN_PATH', VIRTUAL_PATH); # Could change to sub folder
 
 ob_start();  #buffers our page to be prevent header errors. Call before INC files or ANY html!
 header("Cache-Control: no-cache");header("Expires: -1");#Helps stop browser & proxy caching
 
+# END BOOTSTRAP CODE, START SITE SPECIFIC DATA ---------------------------
 
 
-//This defines the current file name
-define ('THIS_PAGE',basename($_SERVER['PHP_SELF']));
-
-//echo THIS_PAGE;
+/*
+START PLACEMENT OF switch AND $nav1!--------------
 
 //this allows us to add unique info to our pages
 switch(THIS_PAGE)
 {
-    case"experiment.php":
-        $title = "My Experiment Title Tag";
-        $pageID = "My Experiment Page ID";
+    case "template.php":
+        $title = "My Template Title Tag";
+        $pageID = "My Template Page ID";
         break;
-
-      case"daily.php":
-        $title = "My Daily Title Tag";
-        $pageID = "My Daily Page ID";
-        break;    
         
+    case "daily.php":
+        $title = "Daily Special!";
+        $pageID = "Daily Special";
+        break;
+        
+    case "contact.php":
+        $title = "Contact Page";
+        $pageID = "Contact us!";
+        break;     
+
+    case "customers.php":
+        $title = "Title for customers page!";
+        $pageID = "Customers";
+        break;         
+
     default:
         $title = THIS_PAGE;
         $pageID = "Retro Diner";
-        
-        
-      
-        
-
 }//end switch
 
-//here are our navigation pages:
-
-$nav1['index.php'] = 'Home Page';
-$nav1['experiment.php'] = 'Home';
+//Here are our navigation pages:
+$nav1['index.php'] = 'Home';
+$nav1['template.php'] = 'Template';
 $nav1['daily.php'] = 'Daily';
+$nav1['customers.php'] = 'Customers';
 $nav1['contact.php'] = 'Contact';
 
-/*foreach($nav1 as $link => $label)
-{
-    echo "link is $link and label is $label <br />";
-
-}
-
-<ul class="navigation">
-				
-				<li>
-					<a href="index.html">Home</a>
-				</li><li>
-					<a class="active" href="about.html">About</a>
-				</li>
-				<li>
-					<a href="burger.html">Menu</a>
-				</li>
-				<li>
-					<a href="contact.html">Contact</a>
-				</li>
-				<li>
-					<a href="blog.html">Blog</a>
-				</li>
-			</ul>
-
-
+END PLACEMENT OF switch AND $nav1!--------------
 */
-
-
-
-//echo $title;
-
-
-
-//die;
 
 /*
-Create links inside the header.php file
-
-<li><a href="LINK">LABEL</a></li>
-<li><class="active"><a href="LINK">LABEL</a></li>
-
-
-
-
+ * adminWidget allows clients to get to admin page from anywhere
+ * code will show/hide based on logged in status
 */
-function makeLinks($arr,$prefix='',$suffix='',$exception='')
-{
-        $myReturn = '';
-        foreach($arr as $link => $label)
-    {  $myReturn .= $exception . '<a href="' . $link . '">' . $label . '</a>' . $suffix; 
-            
-        if(THIS_PAGE == $link){ 
-            //current file gets active class
-        }else{
-        
-             $myReturn .= $prefix . '<a href="' . $link . '">' . $label . '</a>' . $suffix;
-        
-        }
-    }    
-            
+if(startSession() && isset($_SESSION['AdminID']))
+{#add admin logged in info to sidebar or nav
+	$adminWidget = '<li><a href="' . ADMIN_PATH . 'admin_dashboard.php">ADMIN</a></li>';
+	$adminWidget .= '<li><a href="' . ADMIN_PATH . 'admin_logout.php">LOGOUT</a></li>';
+}else{//show login (YOU MAY WANT TO SET TO EMPTY STRING FOR SECURITY)
+    $adminWidget = '<li><a href="' . ADMIN_PATH . 'admin_login.php">LOGIN</a></li>';
+}
 
-        return $myReturn;
-
-}//end makeLinks()
+/*
+ * These variables, when added to the header.php and footer.php files, 
+ * allow custom JS or CSS scripts to be loaded into <head> element and 
+ * just before the closing body tag, respectively
+ */
+$loadhead = '';
+$loadfoot = '';
